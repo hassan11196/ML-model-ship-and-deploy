de76fff050ef8f55ddd9874c7b5e31b87ff2d1f8
Muhammad Hassan Ahmed <33365802+hassan11196@users.noreply.github.com>
2023-11-02 21:35:01 +0000

    Remove unused imports and docker-compose file for testing

--- api/main.py
+++ api/main.py
@@ -1,14 +1,6 @@
-import numpy as np
-import pandas as pd
 import os
-from typing import List
 
-from fastapi import FastAPI, File, UploadFile, HTTPException, Depends
-from pydantic import BaseModel, ValidationError, validator
-from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
-
-# from .ml.model import Model, get_model, n_features
-
+from fastapi import FastAPI
 
 
 app = FastAPI(port=os.environ.get('PORT'))
